2c7be3668a3fefd47c2f5937db62330070911114
janethcps58-dotcom <janethcps58@gmail.com>
2026-04-19 18:03:13 -0500

    train .py

--- src/train .py
+++ src/train .py
@@ -1,1 +1,10 @@
 
+import joblib
+from sklearn.linear_model import LogisticRegression
+
+def train(X_train, y_train, model_path="model.pkl"):
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    joblib.dump(model, model_path)
+    print("Modelo entrenado y guardado en", model_path)
+
