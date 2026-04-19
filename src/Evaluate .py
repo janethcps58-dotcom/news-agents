1a0d45333a6bb904c002ccc2de1a79eebbc5ed8d
janethcps58-dotcom <janethcps58@gmail.com>
2026-04-19 17:55:54 -0500

    Evaluate .py

--- src/Evaluate .py
+++ src/Evaluate .py
@@ -1,1 +1,10 @@
 
+
+import joblib
+from sklearn.metrics import accuracy_score
+
+def evaluate(X_test, y_test, model_path="model.pkl"):
+    model = joblib.load(model_path)
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
+    print("Accuracy:", acc)
